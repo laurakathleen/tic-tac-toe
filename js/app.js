2c1574510d5e0c93602737a414a478ae67abd014
laurakathleen <laurakwalley@gmail.com>
2016-08-19 16:20:34 -0700

    adds reset btn & x's

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
 // wait for the DOM to finish loading
+var box = $('.box');
+var x = $('x');
+var o =  $('o');
+var resetBtn = $('.reset-btn');
+
 $(document).ready(function() {
-  // all code to manipulate the DOM
-  // goes inside this function
+// all code to manipulate the DOM
+// goes inside this function
+	$(box).on('click', function xTurn(){
+		$(this).append('<p>X</p>');
+	})
+
+  	$(resetBtn).on('click', function emptyGrid(){
+  		$(box).empty();
+  	})
+
 
 });

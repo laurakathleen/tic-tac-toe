d2d90999067f5941d2d83c963c78b86597f542d1
laurakathleen <laurakwalley@gmail.com>
2016-08-20 17:54:05 -0700

    fixs dbl append, turn counter

--- js/app.js
+++ js/app.js
@@ -3,17 +3,33 @@ var box = $('.box');
 var x = $('x');
 var o =  $('o');
 var resetBtn = $('.reset-btn');
+var row = $('.row');
+var clickCounter = 0;
+var oneTurn = $('#player1-turn');
+var twoTurn = $('#player2-turn');
 
 $(document).ready(function() {
-// all code to manipulate the DOM
-// goes inside this function
-	$(box).on('click', function xTurn(){
-		$(this).append('<p>X</p>');
+//Keeps track of if an odd turn (player 1) or even turn (player 2)
+	$(box).on('click', function countEm(){
+		//for (var i = 0; i<=9; i++){
+		clickCounter +=1;
+		console.log(clickCounter);
+		return clickCounter;
 	})
-
-  	$(resetBtn).on('click', function emptyGrid(){
-  		$(box).empty();
-  	})
-
-
-});
+//at 0 turns, say 'player 1, go':
+		if (clickCounter == 0 || clickCounter % 2 != 0) {
+			$(oneTurn).css('visibility', 'visible');
+			$(twoTurn).css('visibility', 'hidden');
+		} else {
+			$(twoTurn).css('visibility', 'visible');
+			$(oneTurn).css('visibility', 'hidden');
+		}
+//Add the 'x' for player 1's turn
+		$(box).on('click', function xTurn(){
+		$(this).append('<p>X</p>');
+  		})
+//Reset button
+  		$(resetBtn).on('click', function emptyGrid(){
+  			$(box).empty();
+  		})
+ })

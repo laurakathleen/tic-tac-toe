7f3ea409f028b74cf158e6fc97a22149e09b01b0
laurakathleen <laurakwalley@gmail.com>
2016-08-21 12:28:19 -0700

    edits js for alternating player turn

--- js/app.js
+++ js/app.js
@@ -7,6 +7,8 @@ var row = $('.row');
 var clickCounter = 0;
 var oneTurn = $('#player1-turn');
 var twoTurn = $('#player2-turn');
+var player1;
+var player2;
 
 $(document).ready(function() {
 //Keeps track of if an odd turn (player 1) or even turn (player 2)
@@ -17,19 +19,25 @@ $(document).ready(function() {
 		return clickCounter;
 	})
 //at 0 turns, say 'player 1, go':
+	function switchTurn(){
 		if (clickCounter == 0 || clickCounter % 2 != 0) {
+			player1 = true;
 			$(oneTurn).css('visibility', 'visible');
 			$(twoTurn).css('visibility', 'hidden');
-		} else {
+		} else if (clickCounter % 2 == 0) {
+			player2=true;
 			$(twoTurn).css('visibility', 'visible');
-			$(oneTurn).css('visibility', 'hidden');
+			$(oneTurn).css('visibility', 'hidden');	
 		}
+	}
 //Add the 'x' for player 1's turn
-		$(box).on('click', function xTurn(){
+	$(box).on('click', function xTurn(){
 		$(this).append('<p>X</p>');
-  		})
+		switchTurn();
+	})
 //Reset button
   		$(resetBtn).on('click', function emptyGrid(){
   			$(box).empty();
+  			clickCounter = 0;
   		})
  })

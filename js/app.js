53b90a8ccafc0a6ec37962a28169377305b076ca
laurakathleen <laurakwalley@gmail.com>
2016-08-21 18:42:11 -0700

    Cleans up js code with comments

--- js/app.js
+++ js/app.js
@@ -14,10 +14,11 @@ $(document).ready(function() {
 	$(oneTurn).css('visibility', 'visible');
 //Keeps track of if an odd turn (player 1) or even turn (player 2)
 	$(box).on('click', function countEm(){
-		//for (var i = 0; i<=9; i++){
-		clickCounter +=1;
-		console.log(clickCounter);
-		return clickCounter;
+		for (var i = 0; i<=9; i++){
+			clickCounter +=1;
+			console.log(clickCounter);
+			return clickCounter;
+		} 
 	})
 //at 0 turns, say 'player 1, go':
 	$(box).on('click', function switchTurn(){
@@ -25,19 +26,24 @@ $(document).ready(function() {
 			player2 = true;
 			$(twoTurn).css('visibility', 'visible');
 			$(oneTurn).css('visibility', 'hidden');
+//Check for a claimed box:
 			if ($(this).hasClass('.claimed-box')){
 				alert("Pick a different box.");
 			} else {
+//Marks box with an X:				
 				$(this).append('<p>X</p>');
 				$(this).addClass('.claimed-box');
-			}	
+			}
+//switch to player 2 turn:	
 		} else if (clickCounter == 0 || clickCounter % 2 == 0) {
 			player1=true;
 			$(oneTurn).css('visibility', 'visible');
 			$(twoTurn).css('visibility', 'hidden');
+//Check for a claimed box:
 			if ($(this).hasClass('.claimed-box')){
 				alert("Pick a different box.");
-			} else {	
+			} else {
+//Marks box with an O:		
 				$(this).append('<p>O</p>');
 				$(this).addClass('.claimed-box');
 			}
